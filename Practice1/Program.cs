b463833ba3d1dfa2eb7356c631728b68ad2ec20f
DeFire04 <defire04st@gmail.com>
2022-09-17 14:57:59 +0300

    finished practice 1 and 2

---
+++ Practice1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          
+            double I;
+            double U;
+            double R;
+
+            string strU;
+            string strR;
+
+            bool isUEnteredCorrectly;
+            bool isREnteredCorrectly;
+
+            Console.WriteLine("I = U / R\n" +
+                " R- оми\n U - вольти\n I - ампери");
+            do
+            {
+                Console.Write("Введiть напругу (U): ");
+                strU = Console.ReadLine();
+                isUEnteredCorrectly = double.TryParse(strU, out U);
+                if (!isUEnteredCorrectly)
+                {
+                    Console.WriteLine("Напруга введена некорректно!");
+                }
+            }
+            while (!isUEnteredCorrectly);
+
+            do
+            {
+                Console.Write("Введiть опiр (R):");
+                strR = Console.ReadLine();
+                isREnteredCorrectly = double.TryParse(strR, out R);
+                if (!isREnteredCorrectly)
+                {
+                    Console.WriteLine("Опiр введен некорректно!");
+                }
+            }
+            while (!isREnteredCorrectly);
+
+
+            if (R <= 0)
+            {
+                Console.WriteLine("Погана людина!");
+                Console.ReadLine();
+                return;
+            }
+            I = U / R;
+            Console.WriteLine("I = {0} / {1}\nI = {2}", U, R, I);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn;
+            int nk;
+
+
+            double sum = 0;
+
+
+            nn = int.Parse(Console.ReadLine());
+            nk = int.Parse(Console.ReadLine());
+
+            for (int k = nn; k < nk; k++)
+            {
+                sum += (Math.Pow(k, 2) + Math.Pow(-1, (k - 1)) * 2 * k - 1) / (Math.Pow(k, 2) + 8);
+            }
+
+            Console.WriteLine(sum);
+
+
+            Console.ReadKey();
+        }
+    }
+}
