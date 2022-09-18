0ddc5d007247fcc3b420d77d99039616a30f327f
Dima Olaba <defire04st@gmail.com>
2022-09-18 19:58:00 +0300

    Did practice 3

---
+++ Practice3/Practice3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Дані координати (як цілі від 1 до 8) двох різних полів шахівниці.
+            // Якщо ферзь за один хід може перейти з одного поля на інше, вивести логічне значення True,
+            // інакше вивести значення False. 
+
+            Queen queen = new Queen(new Point(4, 1), new Point(2, 3));
+
+            Console.WriteLine(Checker.check(queen));
+
+            queen.setEnd(new Point(3, 3));
+
+            Console.WriteLine(Checker.check(queen));
+
+            Console.ReadKey();
+        }
+    }
+
+    interface Chessmen {
+        bool canGoThere();
+    }
+
+    class Queen : Chessmen {
+
+        private Point start;
+        private Point end;
+
+        public Queen(Point start, Point end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public Point getStart()
+        {
+            return this.start;
+        }
+
+        public void setStart(Point newPoint)
+        {
+            this.start = newPoint;
+        }
+
+        public Point getEnd()
+        {
+            return this.end;
+        }
+
+        public void setEnd(Point newPoint)
+        {
+            this.end= newPoint;
+        }
+
+
+        public bool canGoThere()
+        {
+            if (Math.Abs(start.getX() - end.getX()) == Math.Abs(start.getY() - end.getY())
+                 || start.getX() == end.getX()
+                 || start.getY() == end.getY())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class Checker {
+        public static bool check(Queen queen) {
+            return queen.canGoThere();
+        }
+    }
+
+    class Point {
+        private int x;
+        private int y;
+
+        
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return this.x;
+        }
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return this.y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+    }
+}
---
+++ Practice3/Practice3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Practice3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practice3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9d12b807-5aa5-483d-8c91-a81cc607af10")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
