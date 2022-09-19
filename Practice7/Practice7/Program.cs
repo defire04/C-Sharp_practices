44ca2104b31262eaf2ebed5ed4677d838f8ad7f0
Dima Olaba <defire04st@gmail.com>
2022-09-19 15:42:19 +0300

    Did practice 7

---
+++ Practice7/Practice7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Practice7
+{
+    class Program {
+        static void Main(string[] args) {
+
+            //Створити ліст стрінгових значень, дозволити можливість заповнення з клавіатури. Вивести кількість елементів рівної двожини.
+
+            List<string> strList = new List<string>();
+            string newElement;
+
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            while (true) {
+
+                Console.Write("Enter new list element(if you want to finish write 'END'): ");
+                newElement = Console.ReadLine();
+
+                if (newElement.Equals("END")) {
+                    break;
+                }
+
+                strList.Add(newElement);
+            }
+
+            int count = 0;
+            foreach(string i in strList) {
+                foreach(string j in strList) {
+                        if(i.Length == j.Length) {
+                            count++;
+                        }
+                }
+
+                if (count > 1) {
+                    if (!result.ContainsKey(i.Length)) {
+                        result.Add(i.Length, count);
+                    }
+                }
+                count = 0;
+            }
+
+
+            foreach (var i in result) {
+                Console.WriteLine($"elements with length {i.Key} equals {i.Value}");
+            }
+
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice7/Practice7/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Practice7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practice7")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fd441cce-5332-46b2-aa6c-4bdd6bfc8824")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
