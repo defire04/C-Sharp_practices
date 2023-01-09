a89aafa838f6d35212bcc5423fed667774e41341
Dima Olaba <defire04st@gmail.com>
2023-01-09 18:16:10 +0200

    Rename Practice 8(with phones)

---
+++ Practice10/Practice10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
